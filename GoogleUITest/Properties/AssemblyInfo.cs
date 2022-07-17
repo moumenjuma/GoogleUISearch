0761f5608e047d05a9ae2173bde4ae1ab0e83344
Moumen Juma <Momenjuma@gmail.com>
2022-07-17 14:08:47 -0400

    Commit Code

---
+++ GoogleUITest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("GoogleUITest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("GoogleUITest")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e3e6ad56-45a2-4f67-baff-449568e891f5")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ GoogleUITest/UnitTest1.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Threading;
+
+namespace GoogleUITest
+
+{
+    [TestClass]
+    public class UnitTest1
+    {
+       
+
+        [TestMethod]
+        public void TestSearch()
+        {
+            string search="Black Ops cold war";//input what you want to search up 
+            int waitingTime = 200;
+            int waitingTimeRead = 2000;
+
+            By googleSearchBar = By.Name("q");//Identifies Googles Searchbar
+            By googleSearchButton = By.Name("btnK");//Identifies Googles Search button
+
+            IWebDriver webDriver = new ChromeDriver();//opens ChromeDriver
+
+            Thread.Sleep(waitingTime);
+
+            webDriver.Navigate().GoToUrl("https://www.google.com");//Navigates to Google
+
+            Thread.Sleep(waitingTime);
+
+            webDriver.Manage().Window.Maximize();//Maximizes Window
+
+            Thread.Sleep(waitingTime);
+
+            webDriver.FindElement(googleSearchBar).SendKeys(search);//enters what is in the string->search 
+
+            Thread.Sleep(waitingTime);
+
+            webDriver.FindElement(googleSearchButton).Click();//clicks on the Search button in google
+
+            Thread.Sleep(waitingTime);
+
+            Thread.Sleep(waitingTimeRead);
+
+            webDriver.Quit();//closes Chrome
+        }
+    }
+}
